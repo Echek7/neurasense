5bcba13321eb213e399ad3ac6d2862479fc47f7b
Echek7 <149231438+Echek7@users.noreply.github.com>
2025-10-25 03:32:40 -0600

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,19 @@
+import sys
+import os
 print(">>> main.py cargado")
+
+# Agrega la ruta backend/app al path de Python
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routes.ping import ping_router
-from .routes.projects import projects_router
+from routes.ping import ping_router
+from routes.projects import projects_router
 
 app = FastAPI(title="NeuraSense API", version="0.1.0")
 
 # CORS para frontend
-origins = ["http://localhost:3000"]  # ajusta si tu frontend está en otra URL
+origins = ["http://localhost:3000"]  # Ajusta según frontend real
 
 app.add_middleware(
     CORSMiddleware,
