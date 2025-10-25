194eddcc08df51f7f47487beeba2e2521fb0c5c3
Echek7 <149231438+Echek7@users.noreply.github.com>
2025-10-25 03:00:39 -0600

    Update supabase_client.py

--- backend/app/services/supabase_client.py
+++ backend/app/services/supabase_client.py
@@ -1,8 +1,7 @@
-# backend/app/supabase_client.py
 from supabase import create_client, Client
 import os
 
-SUPABASE_URL = os.getenv("SUPABASE_URL") or "https://tu-proyecto.supabase.co"
-SUPABASE_KEY = os.getenv("SUPABASE_KEY") or "tu-api-key"
+SUPABASE_URL = os.getenv("SUPABASE_URL") or "https://fyxvkustmfelhvseentt.supabase.co"
+SUPABASE_KEY = os.getenv("SUPABASE_KEY") or "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5eHZrdXN0bWZlbGh2c2VlbnR0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MTI4MTYyNywiZXhwIjoyMDc2ODU3NjI3fQ.3p7248sPdRG-YuFdCHSYs1Je1m9DCh_7Zr8nsiecPlA"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
